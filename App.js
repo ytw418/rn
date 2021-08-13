55c1023143f62b1943db461b315cd9576d25e676
ytw418 <ytw418@gmail.com>
2021-08-13 17:15:49 +0900

    1. 상단 네비게이션 제작
    2. 하단 네비게이션 탭 기능 추가
    
    3. 네이게이션 라이브러리 기능 사용시 모듈을 찾을 수 없는 오류 수정
    
    4. MainActiviy 와 SubActiviy 생성 후 화면간 이동가능한 버튼 기능을 구현하고 뒤로가기 버튼과 초기화면으로 돌아가는 버튼 생성
    
    5. 화면간 이동시 매개변수를 전달하고 받으며 전달받은 매개변수에 랜덤 값을 주어 다시 리턴해주는 코드 작성

--- App.js
+++ App.js
@@ -1,9 +1,27 @@
 import * as React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, TextInput, Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-function HomeScreen({ navigation }) {
+// 헤더 로고 
+function LogoTitle() {
+  return (
+    <Image
+      style={{ width: 50, height: 50 }}
+      source={require('./assets/icon.png')}
+    />
+  );
+}
+
+// 화면간 이동 및 택스트 작성 후 택스트를 반환하면서 다시 이동
+function HomeScreen({ navigation, route }) {
+  React.useEffect(() => {
+    if (route.params?.post) {
+      // Post updated, do something with `route.params.post`
+      // For example, send the post to the server
+    }
+  }, [route.params?.post]);
+
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
@@ -11,6 +29,11 @@ function HomeScreen({ navigation }) {
         title="Go to Details"
         onPress={() => navigation.navigate('Details',{itemId: 86, otherParam: 'anything you want here',})}
       />
+       <Button
+        title="Create post"
+        onPress={() => navigation.navigate('CreatePost')}
+      />
+      <Text style={{ margin: 10 }}>Post: {route.params?.post}</Text>
     </View>
   );
 }
@@ -35,14 +58,71 @@ function DetailsScreen({route, navigation }) {
   );
 }
 
+
+// 화면간 이동 및 택스트 작성 후 택스트를 반환하면서 다시 이동
+
+function CreatePostScreen({ navigation, route }) {
+  const [postText, setPostText] = React.useState('');
+
+  return (
+    <>
+      <TextInput
+        multiline
+        placeholder="What's on your mind?"
+        style={{ height: 200, padding: 10, backgroundColor: 'white' }}
+        value={postText}
+        onChangeText={setPostText}
+      />
+      <Button
+        title="Done"
+        onPress={() => {
+          // Pass and merge params back to home screen
+          navigation.navigate({
+            name: 'Home',
+            params: { post: postText },
+            merge: true,
+          });
+        }}
+      />
+    </>
+  );
+}
+
+
 const Stack = createNativeStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator initialRouteName="Home"   
+       screenOptions={{
+        headerStyle: {
+          backgroundColor: '#000',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      }}> 
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={DetailsScreen}   options={{ title: 'Overview' }} />
+        <Stack.Screen name="Details" component={DetailsScreen}
+        options={{
+          title: 'My home',
+          headerStyle: {
+            backgroundColor: '#f4511e',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }} />
+        <Stack.Screen name="CreatePost" component={CreatePostScreen} options={{ headerTitle: (props) => <LogoTitle {...props} />,          headerRight: () => (
+            <Button
+              onPress={() => alert('This is a button!')}
+              title="Info"
+              color="#fff"
+            />
+          ), }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
